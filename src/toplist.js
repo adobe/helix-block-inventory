5e17b28aef3a45d7fbbac806fef074b25c5a1082
Lars Trieloff <trieloff@adobe.com>
2022-02-15 11:08:36 +0100

    feat(toplist): generate examples for blocks derived from RUM data
    fixes #7

--- src/toplist.js
+++ src/toplist.js
@@ -10,6 +10,8 @@
  * governing permissions and limitations under the License.
  */
 import { fetch } from '@adobe/helix-fetch';
+import { JSDOM } from 'jsdom';
+import Block from './block.js';
 
 export default class TopList {
   constructor(base, serviceurl) {
@@ -19,37 +21,73 @@ export default class TopList {
     this.base.searchParams.set('checkpoint', 'viewblock');
     this.base.searchParams.set('domain', this.original.hostname + this.original.pathname);
     this.serviceurl = serviceurl;
+    this.pages = {};
   }
 
   async fetch() {
     const res = await fetch(this.base.href);
     if (res.ok) {
       this.body = await res.json();
+      this.pages = await this.body.results.reduce(async (p, v) => {
+        const r = await p;
+        if (r[v.topurl]) {
+          return r;
+        }
+        const base = new URL(v.topurl);
+        base.pathname = `${base.pathname}.plain.html`;
+        try {
+        // eslint-disable-next-line no-param-reassign
+          r[v.topurl] = await (await fetch(base.href)).text();
+        } catch {
+          // do nothing
+        }
+        return r;
+      }, {});
       return this;
     }
     throw new Error(`unable to fetch inventory from ${this.base}: ${await res.text()}`);
   }
 
   parse() {
-    this.data = this.body.results;
+    this.data = this.body.results.map((entry) => {
+      const enriched = entry;
+      if (this.pages[entry.topurl]) {
+        enriched.dom = new JSDOM(this.pages[entry.topurl]).window.document;
+      }
+      return enriched;
+    });
     return this;
   }
 
   get blocks() {
-    return this.data.map((e) => ({
-      name: e.source.replace(/^\./, ''),
-      variant: e.source.replace(/^\./, '').split('--').slice(1).map((vi) => vi.replace(/-$/, ''))
-        .join(', '),
-      example: undefined, // tbd
-      preview: (() => {
-        const u = new URL(this.serviceurl);
-        u.searchParams.set('selector', `*[data-block-name="${e.source.replace(/^\./, '')}"]`);
-        u.searchParams.set('inventory', e.topurl);
-        return u.href;
-      })(),
-      title: e.source.replace(/^\./, '').replace(/--.*$/, ''),
-      selector: `*[data-block-name="${e.source.replace(/^\./, '')}"]`,
-      description: '',
-    }));
+    return this.data.map((e) => {
+      const className = e.source.replace(/^\./, '');
+      // wait
+      const el = e.dom.querySelector(`div[class="${className}"]`);
+
+      if (el) {
+        const block = new Block(e.dom, el);
+        return {
+          name: className,
+          variant: block.variant,
+          example: el.outerHTML,
+          table: block.table,
+          preview: (() => {
+            const u = new URL(this.serviceurl);
+            u.searchParams.set('selector', `*[data-block-name="${className}"]`);
+            u.searchParams.set('inventory', e.topurl);
+            return u.href;
+          })(),
+          title: className.replace(/--.*$/, ''),
+          selector: `*[data-block-name="${className}"]`,
+          description: '',
+        };
+      } else {
+        return {
+          synthetic: true,
+          className,
+        };
+      }
+    }).filter((e) => e.synthetic !== true);
   }
 }
